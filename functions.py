e7c1aa54eee88597a47facc67f5ec9830bd5707b
Apostu Andrei <apostu2013@gmail.com>
2024-01-13 17:07:59 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """
+    Read a text file an return the list of todos item.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """Write the todos items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
---
+++ web.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    to_do = st.session_state["new_todo"] + "\n"
+    todos.append(to_do)
+    functions.write_todos(todos)
+
+
+st.title("My Todo App")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="Enter Todo:", label_visibility="hidden",
+              placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
